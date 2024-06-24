329397e0dbcb1c5681615282ad2d7897d7e36c4c
Yajushanjali15 <145789045+Yajushanjali15@users.noreply.github.com>
2024-06-24 17:20:59 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,58 @@
+const questions = [
+    {
+  number:1,        
+  question:'What does HTML Stand for?', 
+  answer:'B) Hyper Text Markup Languagge', 
+  options:[
+          'A) Home Tool Markup Laguagge', 
+          'B) Hyper Text Markup Languagge', 
+          'C) Hyperlinks and Text Markup Languagge', 
+          'D) Hyper Tool Markup Laguagge'
+          ]
+    }, 
+    {
+        number:2,
+        question: 'Which HTML tag is used to create a hyperlink?',
+        answer: 'B) a',
+        options: [
+      'A) link',
+      'B) a',
+      'C) href',
+      'D) url'
+      ]
+}, 
+    {
+            number:3,
+            question: 'Which SQL command is used to retrieve data from a database table?',
+            answer: 'A) SELECT',
+            options: [
+          'A) SELECT',
+          'B) UPDATE',
+          'C) DELETE',
+          'D) INSERT'
+          ]
+    }, 
+    
+    {
+            number:4,
+            question: 'Which of the below CSS class is used to change the text color of CSS ?',
+            answer: 'C) color',
+            options: [
+          'A) background-color',
+          'B) color-background',
+          'C) color',
+          'D) None of the above'
+          ]
+    }, 
+    {
+            number:5,
+            question:'Which keyword is used to declare a variable in JavaScript?',
+            answer:'D) All of the above',
+            options:[
+          'A) var',
+          'B) let',
+          'C) const',
+          'D) All of the above'
+          ]
+    }, 
+ ]
